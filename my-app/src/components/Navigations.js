2bc5fed85c68792fc239facbda9c51240168983f
rae-glazier <glazier.home.life@gmail.com>
2020-03-02 12:04:58 -0600

    ...

--- my-app/src/components/Navigations.js
+++ my-app/src/components/Navigations.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 
 export const Navigation = () => {
     console.log('navigation running')
     return (
         <div>
-
+            <Link></Link>
         </div>
     )
 }
