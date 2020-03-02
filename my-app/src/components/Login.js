6ae9cf3c00fc04c2c17b6bc969b0e37a0d2d2011
rae-glazier <glazier.home.life@gmail.com>
2020-03-02 12:55:02 -0600

    basic login and registration form built

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Login = () => {
+
+    const [login, setLogin] = useState({username:'', password:''});
+
+    const handleChanges = (e) => {
+        setLogin({...login, [e.target.name]: e.target.value})
+    };
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        setLogin({username:'', password:''});
+        console.log('Username:', login.username, 'Password:', login.password)
+    };
+
     return(
         <div>
             <h1>Login</h1>
+            <form onSubmit={submitForm}>
+                <label htmlFor='username'>Username: </label>
+                <input 
+                    id='username'
+                    type='username'
+                    name='username'
+                    required='true'
+                    onChange={handleChanges}
+                    value={login.username}
+                />
+                <br/>
+                <label htmlFor='password'>Password: </label>
+                <input
+                    id='password'
+                    type='password'
+                    name='password'
+                    required='true'
+                    onChange={handleChanges}
+                    value={login.password}
+                />
+                <br/>
+                <button type='submit'>Login</button>
+            </form>
         </div>
     )
 }
--- my-app/src/components/Register.js
+++ my-app/src/components/Register.js
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export const Register = () => {
+
+    const [register, setRegister] = useState({name:'', email:'', username:'', password:''});
+
+    const handleChanges = (e) => {
+        setRegister({...register, [e.target.name]: e.target.value})
+    };
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        setRegister({name:'', email:'', username:'', password:''});
+        console.log('Name:', register.name, 'Email:', register.email, 'Username:', register.username, 'Password:', register.password)
+    };
+
     return(
         <div>
             <h1>Register</h1>
+            <form onSubmit={submitForm}>
+            <label htmlFor='name'>Name: </label>
+                <input 
+                    id='name'
+                    type='text'
+                    name='name'
+                    required='true'
+                    onChange={handleChanges}
+                    value={register.name}
+                />
+                <br/>
+                <label htmlFor='email'>Email: </label>
+                <input 
+                    id='email'
+                    type='email'
+                    name='email'
+                    required='true'
+                    onChange={handleChanges}
+                    value={register.email}
+                />
+                <br/>
+                <label htmlFor='username'>Username: </label>
+                <input 
+                    id='username'
+                    type='username'
+                    name='username'
+                    required='true'
+                    onChange={handleChanges}
+                    value={register.username}
+                />
+                <br/>
+                <label htmlFor='password'>Password: </label>
+                <input
+                    id='password'
+                    type='password'
+                    name='password'
+                    required='true'
+                    onChange={handleChanges}
+                    value={register.password}
+                />
+                <br/>
+                <button type='submit'>Register</button>
+            </form>
         </div>
     )
 }
