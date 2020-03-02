467d0435169898d051ab3f533c8b5ae3e87bc2a1
rae-glazier <glazier.home.life@gmail.com>
2020-03-02 16:32:06 -0600

    working on getting axios calls to work (dummy)

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
+import axios from 'axios';
 
 //styled components
 const LoginStyle = styled.section`
--- my-app/src/components/Register.js
+++ my-app/src/components/Register.js
@@ -1,34 +1,6 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
-import styled from 'styled-components';
 
-//styled components
-const RegStyle = styled.section`
-    margin: 1%;
-    padding: 2%;
-    border: 1px dashed white;
-`
-const Form = styled.p`
-    border: 1px dashed white;
-    padding: 3%;
-    margin: 3%;
-    background: #36454F;
-    color: #e3dac9;
-
-`
-const Field = styled.span`
-    margin: 1%;
-    display: flex;
-    justify-content: space-evenly;
-`
-const Button = styled.span`
-    border: 1px solid white;
-    margin: 3%;
-    padding: 1%;
-    border-radius: 15%; 
-    background: #36454F;
-    color: #e3dac9;   
-`
 
 export const Register = () => {
 
@@ -38,61 +10,46 @@ export const Register = () => {
         setRegister({...register, [e.target.name]: e.target.value})
     };
 
-    const submitForm = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         setRegister({name:'',username:'', password:''});
         console.log('Name:', register.name,'Username:', register.username, 'Password:', register.password)
     };
 
     return(
-        <RegStyle>
+        <div>
             <h1>Register</h1>
-            <Form>
-                <form onSubmit={submitForm}>
-                    <Field>
-                        <label htmlFor='name'>Name: </label>
-                        <input 
-                            id='name'
-                            type='text'
-                            name='name'
-                            onChange={handleChanges}
-                            value={register.name}
-                        />
-                    </Field>
-                
-                    {/* <br/> */}
-
-                    <Field>
-                        <label htmlFor='username'>Username: </label>
-                        <input 
-                            id='username'
-                            type='username'
-                            name='username'
-                            onChange={handleChanges}
-                            value={register.username}
-                        />
-                    </Field>
-                    
-                    {/* <br/> */}
-
-                    <Field>
-                        <label htmlFor='password'>Password: </label>
-                        <input
-                            id='password'
-                            type='password'
-                            name='password'
-                            onChange={handleChanges}
-                            value={register.password}
-                        />
-                    </Field>
-
-                    {/* <br/> */} 
-                </form>
-            </Form>  
-            <Button>
-                <Link to='/todo' style={{ textDecoration: 'none', color: '#e3dac9' }}>Register</Link>
-            </Button>
-        </RegStyle>
+            <form onSubmit={event => handleSubmit(event)}>
+            <label htmlFor='name'>Name: </label>
+                <input 
+                    id='name'
+                    type='text'
+                    name='name'
+                    onChange={handleChanges}
+                    value={register.name}
+                />
+                <br/>
+                <label htmlFor='username'>Username: </label>
+                <input 
+                    id='username'
+                    type='username'
+                    name='username'
+                    onChange={handleChanges}
+                    value={register.username}
+                />
+                <br/>
+                <label htmlFor='password'>Password: </label>
+                <input
+                    id='password'
+                    type='password'
+                    name='password'
+                    onChange={handleChanges}
+                    value={register.password}
+                />
+                <br/>
+                <Link to='/todo' style={{ textDecoration: 'none', color: 'white' }}><button type='submit'>Register</button></Link>
+            </form>
+        </div>
     )
 }
 
